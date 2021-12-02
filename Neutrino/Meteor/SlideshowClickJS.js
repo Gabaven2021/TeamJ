8beece2058f46f794401e8d061967584019f8d47
Riki Oshima <medamasan1@yahoo.co.jp>
2021-12-02 11:39:50 +0900

    ギャラリーに，「流星写真のスライドショー」を作成しました。

---
+++ Neutrino/Meteor/SlideshowClickJS.js
@@ -0,0 +1,23 @@
+function showimg1(){
+document.imgLink.src = "01.jpg";
+document.getElementById("linkArea").href = "SlideshowClick.html";
+document.getElementById("message").innerHTML = "①流星出現直前";
+}
+
+function showimg2(){
+document.imgLink.src = "02.jpg";
+document.getElementById("linkArea").href = "SlideshowClick.html";
+document.getElementById("message").innerHTML = "②流星出現（流星本体）";
+}
+
+function showimg3(){
+document.imgLink.src = "03.jpg";
+document.getElementById("linkArea").href = "SlideshowClick.html";
+document.getElementById("message").innerHTML = "③緑色の短痕";
+}
+
+function showimg4(){
+document.imgLink.src = "04.jpg";
+document.getElementById("linkArea").href = "Meteor.pdf";
+document.getElementById("message").innerHTML = "④消失間際　　　　　　　　　　　　　　　　　　　　 ↑画像クリックで2001年へ☆彡";
+}
